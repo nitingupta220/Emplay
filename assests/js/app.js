977c3f69854f3af31e767ae8371a71999b7eb73b
nitingupta220 <nitin16@navgurukul.org>
2018-03-05 16:05:21 +0530

    change the style of api call

--- assests/js/app.js
+++ assests/js/app.js
@@ -5,23 +5,36 @@ app.controller("emplayController", ["$scope", "$http", "$timeout", "$interval", 
     'use strict';
 
 
-    $scope.callAtInterval = function () {
+    //    $scope.callAtInterval = function () {
+    //
+    //        $http.get("https://api.github.com/search/repositories?q=" + $scope.searchText)
+    //
+    //            .then(function (response) {
+    //                $scope.mydata = response.data;
+    //            });
+    //
+    //    };
+    //    $interval(function () {
+    //        if ($scope.searchText != undefined) {
+    //            $scope.callAtInterval();
+    //        }
+    //
+    //    }, 5000);
 
+    //    $scope.$watch("searchText", function (newVal, oldVal) {
+    //        if (newVal !== oldVal) {
+    //            $http.get("https://api.github.com/search/repositories?q=" + $scope.searchText)
+    //
+    //                .then(function (response) {
+    //                    $scope.mydata = response.data;
+    //                });
+    //        }
+    //    })
+    $scope.inputChanged = function () {
         $http.get("https://api.github.com/search/repositories?q=" + $scope.searchText)
-
             .then(function (response) {
                 $scope.mydata = response.data;
             });
-
-    };
-    $interval(function () {
-        if ($scope.searchText != undefined) {
-            $scope.callAtInterval();
-        }
-
-    }, 5000);
-
-
-
+    }
 
 }]);
